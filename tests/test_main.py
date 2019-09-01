82db1a94ed0e76e11e9d131fec8f2101a1d47feb
Andrew Arderne <andrew.arderne@navenio.com>
2019-09-01 13:56:10 +0100

    fixed tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,7 +4,7 @@ from home_encrypt import decrypt
 
 
 def test_basic_encrypt():
-    key = generate_key()
+    key = generate_key(5)
     msg_in = 'this coded message'
     cipher_msg = encrypt(msg_in, key)
     msg_out = decrypt(cipher_msg, key)
@@ -12,7 +12,7 @@ def test_basic_encrypt():
 
 
 def test_basic_encrypt_cipher_text():
-    key = generate_key()
+    key = generate_key(5)
     msg_in = 'this coded message'
     cipher_msg = encrypt(msg_in, key)
     assert msg_in != cipher_msg
